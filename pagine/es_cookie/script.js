a3c8f08ceb612609403cd9010db1871f891b9b73
Barbuto Daniel <barbuto.daniel29@hotmail.com>
2026-04-20 09:59:26 +0200

    Nuovo esercizio: Cookie

---
+++ pagine/es_cookie/script.js
@@ -0,0 +1,34 @@
+//un cookie ha una duration
+function setCookie(nome,valore,durataGiorni){
+    let data = new Date();
+    data.setTime(data.getTime()+(giorni * 24 * 60 * 60 * 1000)); // converto da gg in ms
+    document.cookie = nome + "=" 
+                        + encodeURIComponent(valore) // toglie spazi
+                        + ";expires=" + data.toUTCString(); // scadenza (con fuso orario, + preciso)
+                        + ";path=/"
+}
+
+function getCookie(nome){
+
+}
+
+function salvaDati(){
+    let nome = document.getElementById("nome").value;
+    let cognome = document.getElementById("cognome").value;
+
+    setCookie("nome",nome,1);
+    setCookie("cognome",cognome,1);
+
+    window.location.href = "page2.html"; // naviga nella page2
+}
+
+function caricaRiepilogo(){
+    let nome = getCookie("nome");
+    let cognome = getCookie("cognome");
+
+    let s = "";
+    s+="Nome: "+nome;
+    s+="<br>";
+    s+="Cognome: "+cognome;
+    document.getElementById("riepilogo").innerHTML = s; //dentro il div
+}
---
+++ pagine/es_js_2/script.js
@@ -0,0 +1,7 @@
+function Registrati(){
+    let cognome = document.getElementById("txtCognome").value;
+    let nome = document.getElementById("txtNome").value;
+    let code = document.getElementById("txtCodice").value;
+    let sesso = document.getElementsByName();
+
+}
