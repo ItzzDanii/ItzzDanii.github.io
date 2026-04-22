6a0c6483f3020d79156963177205cc509751fd26
Barbuto Daniel <barbuto.daniel29@hotmail.com>
2026-04-22 13:46:35 +0200

    Nuovo esercizio: Quiz Cookies

---
+++ pagine/quiz_cookie/script.js
@@ -0,0 +1,98 @@
+//un cookie ha una duration
+function setCookie(nome,valore,durataGiorni){
+    let data = new Date();
+    data.setTime(data.getTime()+(durataGiorni * 24 * 60 * 60 * 1000)); // converto da gg in ms
+    document.cookie = nome + "=" 
+                        + encodeURIComponent(valore) // toglie spazi
+                        + ";expires=" + data.toUTCString(); // scadenza (con fuso orario, + preciso)
+                        + ";path=/"
+}
+
+function getCookie(nome){
+    let cookies = document.cookie.split(";"); // splitto i cookie in un array
+
+    for (let i=0; i < cookies.length; i++){
+        let c = cookies[i].trim();
+        if (c.startsWith(nome + "=")) {
+            return decodeURIComponent(c.substring(nome.length + 1));
+        }
+    }
+
+    window.location.href = "page2.html" ;
+}
+
+function salvaDati(numero_risposta){
+    switch(numero_risposta){
+        case 1:
+            let r1 = document.getElementById("risp1").value;
+            if(r1 === "")
+                r1 = "Nessuna risposta";
+
+            setCookie("risp1",r1,1);
+            break;
+
+        case 2:
+            let r2 = document.getElementById("risp2").value;
+            if(r2 === "")
+                r2 = "Nessuna risposta";
+
+            setCookie("risp2",r2,1);
+            break;
+
+        case 3:
+            let r3;
+            if(document.getElementById("risp3.1").checked)
+                r3 = "Statiche";
+            else if(document.getElementById("risp3.2").checked)
+                r3 = "Dinamiche";
+            else r3 = "Nessuna risposta";
+
+            setCookie("risp3",r3,1);
+            break;
+
+        case 4:
+            let r4;
+            if(document.getElementById("risp4.1").checked == true)
+                r4 += "void";
+            if(document.getElementById("risp4.2").checked == true)
+                r4 += "integer";
+             if(document.getElementById("risp4.3").checked == true)
+                r4 += "char";
+            else  if(document.getElementById("risp4.1").checked && document.getElementById("risp4.2").checked && document.getElementById("risp4.3").checked)
+                r4 = "Nessuna risposta";
+
+            setCookie("risp4",r4,1);
+            break;
+
+        case 5:
+            let r5 = document.getElementById("risp5").value;
+            if(r5 === "")
+                r5 = "Nessuna risposta";
+
+            setCookie("risp5",r5,1);
+            break;
+    }
+}
+
+function caricaRiepilogo(){
+    let r1 = getCookie("risp1") || "Nessuna risposta";
+    let r2 = getCookie("risp2") || "Nessuna risposta";
+    let r3 = getCookie("risp3") || "Nessuna risposta";
+    let r4 = getCookie("risp4") || "Nessuna risposta";
+    let r5 = getCookie("risp5") || "Nessuna risposta";
+    
+    let s = "";
+
+    s += "<h3>Risposte Quiz</h3>";
+    s += "<strong>Risposta 1: </strong>" + r1 + "<br>";
+    s += "<strong>Risposta 2: </strong>" + r2 + "<br>";
+    s += "<strong>Risposta 3: </strong>" + r3 + "<br>";
+    s += "<strong>Risposta 4: </strong>" + r4 + "<br>";
+    s += "<strong>Risposta 5: </strong>" + r5 + "<br>";
+
+    // Inseriamo tutto nel div con id "riepilogo"
+    let container = document.getElementById("riepilogo");
+    if(container) {
+        container.innerHTML = s;
+    }
+}
