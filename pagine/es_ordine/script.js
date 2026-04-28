c605db0a63de032f8a14b26c9929d3a6b3699b99
DESKTOP-GJSM0CA\barbu <barbuto.daniel29@hotmail.com>
2026-04-28 17:41:52 +0200

    Completato esercizio relativo all'ordine

--- pagine/es_ordine/script.js
+++ pagine/es_ordine/script.js
@@ -1,6 +1,6 @@
-function calcolaTotale(){
-    let tot = 0;
+let tot = 0;
 
+function calcolaTotale(){
     const PREZZO_MOUSE = 16;
     const PREZZO_TAST = 25;
     const PREZZO_STAMP = 350;
@@ -11,9 +11,42 @@ function calcolaTotale(){
     let Qstamp = document.getElementById("txtQStamp").value;
     let Qscan  = document.getElementById("txtQScan").value;
 
-    tot = Math.abs(Qmouse * PREZZO_MOUSE) + Math.abs(Qtast * PREZZO_TAST) + Math.abs(Qstamp * PREZZO_STAMP) + Math.abs(Qscan * PREZZO_SCAN);
+    document.getElementById("txtTotMouse").value = Qmouse;
+    document.getElementById("txtTotTast").value = Qtast;
+    document.getElementById("txtTotStamp").value = Qstamp;
+    document.getElementById("txtTotScan").value = Qscan;
 
-    document.
+    if(Qmouse > 0)
+        tot += Qmouse * PREZZO_MOUSE;
+    if(Qtast > 0)
+        tot += Qtast * PREZZO_TAST;
+    if(Qstamp > 0)
+        tot += Qstamp * PREZZO_STAMP;
+    if(Qscan > 0)
+        tot += Qscan * PREZZO_SCAN;
+
     document.getElementById("txtTot").value = tot;
     console.log("Totale aggiornato:", tot);
+}
+
+function inviaOrdine(){
+    if(!document.getElementById("radioSi").checked && 
+       !document.getElementById("radioNo").checked){
+       window.alert("Seleziona una risposta per le notifiche!");
+       return;
+    }
+
+    if(document.getElementById("txtEmail").value === ""){
+        window.alert("Inserisci email!");
+        return;
+    }
+    
+    else{
+        let msg = "";
+        msg += "ORDINE INVIATO CON SUCCESSO!";
+        msg+="\nTotale: "+tot+"$";
+        msg+="\nEmail: "+document.getElementById("txtEmail").value;
+        msg+="\nPagamento: "+document.getElementById("comboPaga").value;
+        console.log(msg);
+    }
 }
