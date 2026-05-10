62f54fa801873d2a527fb492c015c724b8c0b13d
DESKTOP-GJSM0CA\barbu <barbuto.daniel29@hotmail.com>
2026-05-10 09:13:26 +0200

    Rimosso progetto duplicato

--- pagine/es_js_2/script.js
+++
@@ -1,7 +0,0 @@
-function Registrati(){
-    let cognome = document.getElementById("txtCognome").value;
-    let nome = document.getElementById("txtNome").value;
-    let code = document.getElementById("txtCodice").value;
-    let sesso = document.getElementsByName();
-
-}
